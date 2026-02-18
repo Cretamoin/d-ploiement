e5f5b500587c0f19fbbb37bc4cb6fbc2a7d33968
cretamoin <cretamoin@Fanilo>
2026-02-18 03:31:22 +0300

    login insécurisé

--- js/page.js
+++ js/page.js
@@ -6,11 +6,3 @@ document.getElementById("lien").addEventListener("click", (e) => {
         window.location.href = e.target.href;
     }, 500);
 });
-
-window.addEventListener("load", () => {
-    document.body.classList.add("loaded");
-});
-
-window.onload = () => {
-    document.getElementById("all").style.opacity='1';
-};
