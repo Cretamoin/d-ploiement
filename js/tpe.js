824439ca2f754c2b5928c43ad33bcd956b282dc6
cretamoin <cretamoin@Fanilo>
2026-03-06 09:01:39 +0300

    projet complet

--- js/tpe.js
+++ js/tpe.js
@@ -2,6 +2,7 @@ const nom = document.getElementById("nom");
 
 nom.addEventListener("mouseenter", () => {
   nom.style.backdropFilter = "blur(30px)";
+  nom.style.transition="1s";
 });
 
 nom.addEventListener("mouseleave", () => {
